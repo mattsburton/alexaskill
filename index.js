b5d3cf5d75555f15410824135671e4b1dd3eb583
matt-burton <webape2@gmail.com>
2017-07-13 16:55:20 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+let express = require('express'),
+    bodyParser = require('body-parser'),
+    alexaVerifier = require('alexa-verifier'); // at the top of our file
+
+let app = express();
+
+function requestVerifier(req, res, next) {
+    alexaVerifier(
+        req.headers.signaturecertchainurl,
+        req.headers.signature,
+        req.rawBody,
+        function verificationCallback(err) {
+            if (err) {
+                res.status(401).json({ message: 'Verification Failure', error: err });
+            } else {
+                next();
+            }
+        }
+    );
+}
+
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json({
+    verify: function getRawBody(req, res, buf) {
+        req.rawBody = buf.toString();
+    }
+}));
+
+
+app.get('/', function(req, res) {
+    res.json({ message: 'The forecaster is up and running.', since: (new Date()).toString() });
+});
+
+app.post('/forecast', requestVerifier, function(req, res) {
+  if (req.body.request.type === 'LaunchRequest') { /* ... */ }
+  else if (req.body.request.type === 'SessionEndedRequest') { /* ... */ }
+  else if (req.body.request.type === 'IntentRequest' &&
+           req.body.request.intent.name === 'Forecast') {
+
+    if (!req.body.request.intent.slots.Day ||
+        !req.body.request.intent.slots.Day.value) {
+      // Handle this error by producing a response like:
+      // "Hmm, what day do you want to know the forecast for?"
+    }
+    let day = new Date(req.body.request.intent.slots.Day.value);
+
+    // Do your business logic to get weather data here!
+    // Then send a JSON response...
+
+    res.json({
+      "version": "1.0",
+      "response": {
+        "shouldEndSession": true,
+        "outputSpeech": {
+          "type": "SSML",
+          "ssml": "<speak>Looks like a great day!</speak>"
+        }
+      }
+    });
+  }
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Forecaster is up and running on port %d', app.get('port'));
+});
---
+++ verify.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+function requestVerifier(req, res, next) {
+    alexaVerifier(
+        req.headers.signaturecertchainurl,
+        req.headers.signature,
+        req.rawBody,
+        function verificationCallback(err) {
+            if (err) {
+                res.status(401).json({ message: 'Verification Failure', error: err });
+            } else {
+                next();
+            }
+        }
+    );
+}
