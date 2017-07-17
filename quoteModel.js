de0138901217193936203521557502d6a9ec6323
matt-burton <webape2@gmail.com>
2017-07-17 17:06:23 +0100

    Recommended quote

--- quoteModel.js
+++ quoteModel.js
@@ -1,15 +1,23 @@
 // require mongoose to act as our middleware
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-mongoose.connect('mongodb://heroku_cll2h38q:2qhvjeoq2tc8rr5hd1ll9869m7@ds161742.mlab.com:61742/heroku_cll2h38q');
 
-var quoteSchema = new Schema({ //This is the main schema that defines what is saved in the database - fields/names etc.
-        name : String,
-        quote : String
+/*
+ * Mongoose by default sets the auto_reconnect option to true.
+ * We recommend setting socket options at both the server and replica set level.
+ * We recommend a 30 second connection timeout because it allows for
+ * plenty of time in most operating environments.
+ */
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+
+var mongodbUri = 'mongodb://heroku_cll2h38q:2qhvjeoq2tc8rr5hd1ll9869m7@ds161742.mlab.com:61742/heroku_cll2h38q';
+
+mongoose.connect(mongodbUri, options);
+var conn = mongoose.connection;
+
+conn.on('error', console.error.bind(console, 'connection error:'));
+
+conn.once('open', function() {
+  // Wait for the database connection to establish, then start the app.
 });
-
-
-var quoteModel = mongoose.model('Quote', quoteSchema);
-
-// make this available to our users in our Node applications
-module.exports = quoteModel;
