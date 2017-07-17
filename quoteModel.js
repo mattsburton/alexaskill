c76e5686cc5fa044c07325c28bff1f86b4b6851d
matt-burton <webape2@gmail.com>
2017-07-17 16:58:05 +0100

    Connection change

--- quoteModel.js
+++ quoteModel.js
@@ -1,7 +1,7 @@
 // require mongoose to act as our middleware
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-mongoose.connect('mongodb://127.0.0.1/timeline-app');
+mongoose.connect('mongodb://heroku_cll2h38q:2qhvjeoq2tc8rr5hd1ll9869m7@ds161742.mlab.com:61742/heroku_cll2h38q');
 
 var quoteSchema = new Schema({ //This is the main schema that defines what is saved in the database - fields/names etc.
         name : String,
