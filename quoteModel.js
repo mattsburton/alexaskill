196dcb0a6c257f05783740865ea83aeefb67a389
matt-burton <webape2@gmail.com>
2017-07-18 17:09:30 +0100

    update for atlas

--- quoteModel.js
+++ quoteModel.js
@@ -1,44 +1,15 @@
-// require mongoose to act as our middleware
+// grab the things we need
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+mongoose.connect('mongodb://mattb:mrbarbie9@cluster0-shard-00-00-bwu5m.mongodb.net:27017,cluster0-shard-00-01-bwu5m.mongodb.net:27017,cluster0-shard-00-02-bwu5m.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
 
-/*
- * Mongoose by default sets the auto_reconnect option to true.
- * We recommend setting socket options at both the server and replica set level.
- * We recommend a 30 second connection timeout because it allows for
- * plenty of time in most operating environments.
- */
-var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
-                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
-
-                // Here we find an appropriate database to connect to, defaulting to
-                // localhost if we don't find one.
-                var uristring =
-                  process.env.MONGODB_URI ||
-                  'mongodb://localhost/HelloMongoose';
-
-                // The http server will listen to an appropriate port, or default to
-                // port 5000.
-                var theport = process.env.PORT || 5000;
-
-                // Makes connection asynchronously.  Mongoose will queue up database
-                // operations and release them when the connection is complete.
-                mongoose.connect(uristring, function (err, res) {
-                  if (err) {
-                    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-                  } else {
-                    console.log ('Succeeded connected to: ' + uristring);
-                  }
-                });
-
-                
-                var quoteSchema = new Schema({
-                        name : String,
-                        quote : String
-                });
+var quoteSchema = new Schema({
+        name : String,
+        quote : String
+});
 
 
-                var quoteModel = mongoose.model('Quote', quoteSchema);
+var quoteModel = mongoose.model('Quote', quoteSchema);
 
-                // make this available to our users in our Node applications
-                module.exports = quoteModel;
+// make this available to our users in our Node applications
+module.exports = quoteModel;
