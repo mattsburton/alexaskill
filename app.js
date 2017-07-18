a9c792e2c8403401e7e696c9af96276515fb732b
matt-burton <webape2@gmail.com>
2017-07-18 13:00:11 +0100

    Rewrite

--- app.js
+++ app.js
@@ -1,63 +1,45 @@
-var http = require ('http');	     // For serving a basic web page.
-var mongoose = require ("mongoose"); // The reason for this demo.
+var http = require('http');
 const express = require('express');
 const bodyParser= require('body-parser')
 const app = express()
-// Here we find an appropriate database to connect to, defaulting to
-// localhost if we don't find one.
-var uristring =
-  process.env.MONGODB_URI ||
-  'mongodb://localhost/HelloMongoose';
+var quote = require('./quoteModel');
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+// create a new quote
 
-// The http server will listen to an appropriate port, or default to
-// port 5000.
-var theport = process.env.PORT || 5000;
 
-// Makes connection asynchronously.  Mongoose will queue up database
-// operations and release them when the connection is complete.
-mongoose.connect(uristring, function (err, res) {
-  if (err) {
-    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-  } else {
-    console.log ('Succeeded connected to: ' + uristring);
-  }
+//Save the quote to mongo
+
+//Express routes to save and serve data - we should move this to a sperate file for transparancy
+app.post('/addQuote', (req, res) => {
+  var newQuote = quote({ //This builds up the JSON array from the form
+    name: req.body.name,
+    quote: req.body.quote
+  });
+   newQuote.save(function(err) {
+     if (err) throw err;
+   })
+  res.redirect('/')
+})
+
+
+app.get('/api/quotes', function(req, res) {
+         // use mongoose to get all todos in the database
+         quote.find(function(err, quotes) {
+           if (err)
+               res.send(err)
+           res.json(quotes); // return all todos in JSON format
+         });
 });
-
-// This is the schema.  Note the types, validation and trim
-// statements.  They enforce useful constraints on the data.
-var userSchema = new mongoose.Schema({
-  name: {
-    first: String,
-    last: { type: String, trim: true }
-  },
-  age: { type: Number, min: 0}
-});
-
-// Compiles the schema into a model, opening (or creating, if
-// nonexistent) the 'PowerUsers' collection in the MongoDB database
-var PUser = mongoose.model('PowerUsers', userSchema);
-
-
-// Creating more users manually
-var janedoe = new PUser ({
-  name: { first: 'Jane', last: 'Doe' },
-  age: 65
-});
-janedoe.save(function (err) {if (err) console.log ('Error on save!')});
-
-// In case the browser connects before the database is connected, the
-// user will see this message.
-var found = ['DB Connection not yet established.  Try again later.  Check the console output for error messages if this persists.'];
-
-app.get('/index', function(req, res) {
-    res.sendFile(__dirname + '/index.html'); // load the single view file (angular will handle the page changes on the front-end) Essentially the form!
-});
-// Tell the console we're getting ready.
-// The listener in http.createServer should still be active after these messages are emitted.
-console.log('http server will be listening on port %d', theport);
-console.log('CTRL+C to exit');
 
 // application -------------------------------------------------------------
     app.get('/', function(req, res) {
         res.sendFile(__dirname + '/index.html'); // load the single view file (angular will handle the page changes on the front-end) Essentially the form!
     });
+
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
