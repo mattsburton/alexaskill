6c7a3b4a23b8f1537675f9d54078164c8d5a1ec4
matt-burton <webape2@gmail.com>
2017-07-18 12:49:16 +0100

    Add route

--- app.js
+++ app.js
@@ -1,39 +1,3 @@
-//
-// hello-mongoose: MongoDB with Mongoose on Node.js example on Heroku.
-// Mongoose is a object/data mapping utility for the MongoDB database.
-//
-
-// by Ben Wen with thanks to Aaron Heckmann
-
-//
-// Copyright 2015 ObjectLabs Corp.
-// ObjectLabs operates MongoLab.com a MongoDb-as-a-Service offering
-//
-// MIT Licensed
-//
-
-// Permission is hereby granted, free of charge, to any person
-// obtaining a copy of this software and associated documentation files
-// (the "Software"), to deal in the Software without restriction,
-// including without limitation the rights to use, copy, modify, merge,
-// publish, distribute, sublicense, and/or sell copies of the Software,
-// and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
-//
-// Preamble
 var http = require ('http');	     // For serving a basic web page.
 var mongoose = require ("mongoose"); // The reason for this demo.
 var express = require('express');
@@ -112,7 +76,9 @@ function createWebpage (req, res) {
     };
   });
 }
-
+app.get('/index', function(req, res) {
+    res.sendFile(__dirname + '/index.html'); // load the single view file (angular will handle the page changes on the front-end) Essentially the form!
+});
 // Tell the console we're getting ready.
 // The listener in http.createServer should still be active after these messages are emitted.
 console.log('http server will be listening on port %d', theport);
